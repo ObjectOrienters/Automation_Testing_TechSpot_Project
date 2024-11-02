f94e92b38b73b2b6f3a1bdfc8d80111c9caa56c3
RamHusam111 <husamramoni@gmail.com>
2024-11-02 13:35:28 +0200

    make the login page test cross browser

--- src/test/java/org/example/login/LoginPageTest.java
+++ src/test/java/org/example/login/LoginPageTest.java
@@ -1,17 +1,17 @@
 package org.example.login;
 
-import junit.framework.Assert;
-import org.example.BaseTest;
+import org.example.CrossBrowserBaseTest;
 import org.example.utilities.CsvReader;
 import org.example.utilities.Constants;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.WebElement;
 
 import java.util.Map;
 
-public class LoginPageTest extends BaseTest {
+public class LoginPageTest extends CrossBrowserBaseTest {
     LoginPage loginPage;
 
     @BeforeEach
@@ -19,24 +19,29 @@ public class LoginPageTest extends BaseTest {
         loginPage = new LoginPage(driver);
     }
 
-    @Test
-    public void testLoginToHomePage_Success() {
+    @ParameterizedTest
+    @ValueSource(strings = {"chrome", "safari"})
+    public void testLoginToHomePage_Success(String browser) {
+        setUp(browser);
+        loginPage = new LoginPage(driver);
         Map<String, String> credentials = CsvReader.readCSVFile(Constants.LOGIN_SUCCESS_CSV_FILE_PATH);
         String username = credentials.get("username");
         String password = credentials.get("password");
         loginPage.loginToHomePage(username, password);
         waitUrl(Constants.HOME_URL);
-        Assert.assertEquals(Constants.HOME_URL, driver.getCurrentUrl());
+        Assertions.assertEquals(Constants.HOME_URL, driver.getCurrentUrl());
     }
-    @Test
-    public void testLoginToHomePage_Fail() {
+    @ParameterizedTest
+    @ValueSource(strings = {"chrome", "safari"})
+    public void testLoginToHomePage_Fail(String browser) {
+        setUp(browser);
+        loginPage = new LoginPage(driver);
         Map<String, String> credentials = CsvReader.readCSVFile(Constants.LOGIN_FAIL_CSV_FILE_PATH);
         String username = credentials.get("username");
         String password = credentials.get("password");
         loginPage.loginToHomePage(username, password);
         WebElement errorToast = waitVisibilityOfElementLocated(LoginLocators.LOGIN_ERROR_TOAST);
         Assertions.assertTrue(errorToast.isDisplayed(), LoginLocators.TOAST_ERROR_ISNT_DISPLAYED);
-
 
     }
 }
